e8c9ff156b52d6940cb08641affeee29747866ff
KageRyu7 <Ryan.O.N.Scott@gmail.com>
2017-07-15 13:18:20 -0400

    Renamed app/scripts/ to app/js/

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 var ReactDom = require('react-dom');
 var React = require('react');
-var App = require('./scripts/app.jsx');
+var App = require('./js/app.jsx');
 
 ReactDom.render(<App/>, document.getElementById('main'));
