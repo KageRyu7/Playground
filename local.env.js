fed8891074223c9639be049863b81015098bf6d7
KageRyu7 <Ryan.O.N.Scott@gmail.com>
2017-08-22 13:18:01 -0400

    remove commit when done

---
+++ local.env.js
@@ -0,0 +1,12 @@
+module.exports = {
+    CACHE_CONFIG:'{"host":"http://10.1.94.103:9201"}',
+    CACHE_INDEX:'dig-etk-search',
+    
+    IMAGE_SERVICE_AUTH:'{"user":"1dig2","password":"BotscRHQqalKQIjPAFqK7UI2QNoe88"}',
+    IMAGE_SERVICE_HOST:'{"url":"https://isi.memexproxy.com/ESCORTS/cu_image_search/byURL","base64":"https://isi.memexproxy.com/ESCORTS/cu_image_search/byB64_nocache"}',
+
+    QUERY_URL:'http://52.36.12.77:9877/search/coarse',
+
+    ELASTIC_CONFIG:'{"host":"http://10.1.94.103:9201"}',
+    ELASTIC_INDEX:'dig-etk-search'
+};
